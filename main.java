c9620c2442b7bd1f2dc1ac9e9abda7087b985e88
Keven Gustavo <kevengustavo2012@gmail.com>
2022-11-26 13:49:52 -0300

    Main/Produto

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        produto produto = new produto(1,"Teste");
+        System.out.println("Nome: "+produto.nome+"\nCodigo: "+produto.codigo);
+    }
+}
+  
--- produto.java
+++ produto.java
@@ -1,3 +1,11 @@
 public class produto {
-    
+    int codigo;
+    String nome; 
+    float preço;
+    int quantidade;
+
+    public produto(int Codigo,String Nome){
+        this.codigo =  Codigo;
+        this.nome =  Nome;
+    }
 }
