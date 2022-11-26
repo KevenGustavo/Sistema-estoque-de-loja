2310aec31f73dd529ddc2106c3092f348a1836d7
Keven Gustavo <kevengustavo2012@gmail.com>
2022-11-26 17:29:02 -0300

    Estoque / fornecedor

--- estoque.java
+++ estoque.java
@@ -1,3 +1,8 @@
 public class estoque {
     
+    public void cadastrarProduto(){}
+    public void addProduto(){}
+    public void venderProduto(){}
+    public void excluirProduto(){}
+    public void buscarProduto(){}
 }
--- fornecedor.java
+++ fornecedor.java
@@ -1,3 +1,4 @@
 public class fornecedor {
-    
+    int id;
+    String nome;
 }
