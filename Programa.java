c2c1575ba0377719c4a08493ee2cb31787a3edd6
Keven Gustavo <kevengustavo2012@gmail.com>
2022-11-28 16:32:38 -0300

    controle1

--- Programa.java
+++ Programa.java
@@ -1,7 +1,6 @@
 public class Programa {
     public static void main(String[] args) {
 
-        Estoque controle1 = new Estoque();
         Fornecedores fornecedores = new Fornecedores();
         Estoque controle = new Estoque();
 
