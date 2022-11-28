f7b61e0488a8ba4dbcd4bad8676b8b2e0f01ea0f
Keven Gustavo <kevengustavo2012@gmail.com>
2022-11-28 11:04:00 -0300

    main mod

--- main.java
+++ main.java
@@ -2,9 +2,12 @@ public class Main{
     public static void main(String[] args) {
         fornecedores fornecedores = new fornecedores();
         estoque controle = new estoque();
+
         fornecedores.cadastrarFornecedor("Mercado Livre");
         String mercadoLivre = fornecedores.getFornecedor("Mercado Livre");
+
         controle.cadastrarProduto(true,21,"Limpo",mercadoLivre,12.50f,2);
+        
         controle.listarProdutos();
         controle.setQuantidadeProduto(21, 30);
         controle.listarProdutos();
