410c1f0de44eb8bce7cc824d4c9db9c87c2e9562
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2022-11-28 15:52:09 -0300

    Main

---
+++ Programa.java
@@ -0,0 +1,18 @@
+public class Programa {
+    public static void main(String[] args) {
+
+        Estoque controle1 = new Estoque();
+        Fornecedores fornecedores = new Fornecedores();
+        Estoque controle = new Estoque();
+
+        fornecedores.cadastrarFornecedor("Mercado Livre");
+        String mercadoLivre = fornecedores.getFornecedor("Mercado Livre");
+
+        controle.cadastrarProduto(true,21,"Limpo",mercadoLivre,12.50f,2);
+
+        controle.listarProdutos();
+        controle.setQuantidadeProduto(21, 30);
+        controle.listarProdutos();
+
+    }
+}
